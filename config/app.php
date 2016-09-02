c7c4fbe33eeb0f3e933365dc96d0f1b9330c4626
Sun Doge <sundoge@SundeMacBook-Pro.local>
2016-09-02 17:37:12 +0800

    fix time

--- config/app.php
+++ config/app.php
@@ -64,8 +64,8 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
-
+//    'timezone' => 'UTC',
+    'timezone' => 'Asia/Shanghai',
     /*
     |--------------------------------------------------------------------------
     | Application Locale Configuration
