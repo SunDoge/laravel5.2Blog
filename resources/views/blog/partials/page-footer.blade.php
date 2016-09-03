239220dd4924152a482142cf22bdc70dda65ca24
Sun Doge <sundoge@SundeMacBook-Pro.local>
2016-09-03 12:16:16 +0800

    test

--- resources/views/blog/partials/page-footer.blade.php
+++ resources/views/blog/partials/page-footer.blade.php
@@ -2,7 +2,8 @@
     <hr>
     <div class="container">
         <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-            @include('blog.partials.disqus')
+            {{--@include('blog.partials.disqus')--}}
+            @include('blog.partials.duoshuo')
         </div>
     </div>
 @endif
