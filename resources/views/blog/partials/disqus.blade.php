68188b1c97ca886b4141643e5d630b1df3302baf
Sun Doge <sundoge@SundeMacBook-Pro.local>
2016-09-03 12:07:04 +0800

    xxx

--- resources/views/blog/partials/disqus.blade.php
+++ resources/views/blog/partials/disqus.blade.php
@@ -7,7 +7,7 @@
 
     var disqus_config = function () {
         this.page.url = '{{ url('/') }}';  // Replace PAGE_URL with your page's canonical URL variable
-        this.page.identifier = 'blog-{{ $slug }}; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+        this.page.identifier = 'blog-{{ $slug }}'; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
     };
 
 
